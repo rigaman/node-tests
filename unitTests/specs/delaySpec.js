9a2b02f00819785584963102c05663626f3cb460
Philip <philip@highground.com>
2013-05-05 01:01:55 -0500

    adding q promise

---
+++ unitTests/specs/delaySpec.js
@@ -0,0 +1,8 @@
+var module = require('../../lib/businesslayer/delayTest.js');
+
+describe("This is test to test test harness", function(){
+	it("Expect function to be delayed for 1000 ms", function(){
+
+	});
+
+});
--- unitTests/specs/testSpec.js
+++ unitTests/specs/testSpec.js
@@ -2,13 +2,10 @@ var injector = require('../injector.js');
 var module = injector('./lib/businesslayer/businessTest.js', {
    '../datalayer/dataTest.js' : './lib/datamock/dataTest.js',
 });
-describe("Welcome to jasmine unit tests", function() {
-
-	describe("This is test to test test harness", function(){
-		it("Expect test to be equal 124", function(){
-			var test = module.test();
-			expect(test.id).toBe(124);
-		});
-
+describe("This is test to test test harness", function(){
+	it("Expect test to be equal 124", function(){
+		var test = module.test();
+		expect(test.id).toBe(124);
 	});
+
 });
